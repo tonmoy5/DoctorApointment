68dfde679e4a884f30e61dee370302c7bb00da0a
Md Tonmoym <tonmoy52532@gmail.com>
2022-02-06 20:32:58 +0600

    Admin Topbar

--- admin/home.php
+++ admin/home.php
@@ -1,33 +1,34 @@
 <style>
-   
+
 </style>
 
 <div class="containe-fluid">
 
 	<div class="row">
 		<div class="col-lg-12">
-			
+
 		</div>
 	</div>
 
 	<div class="row mt-3 ml-3 mr-3">
-			<div class="col-lg-12">
+		<div class="col-lg-12">
 			<div class="card">
 				<div class="card-body">
-				<?php echo "Welcome back ".($_SESSION['login_type'] == 3 ? "Dr. ".$_SESSION['login_name'].','.$_SESSION['login_name_pref'] : $_SESSION['login_name'])."!"  ?>
-									
+					<!-- <?php echo "Welcome back " . ($_SESSION['login_type'] == 3 ? "Dr. " . $_SESSION['login_name'] . ',' . $_SESSION['login_name_pref'] : $_SESSION['login_name']) . "!"  ?> -->
+					<h1>Home Page</h1>
+
 				</div>
 				<hr>
 				<div class="row">
-				
+
 				</div>
 			</div>
-			
-		</div>
+
 		</div>
 	</div>
+</div>
 
 </div>
 <script>
-	
+
 </script>
