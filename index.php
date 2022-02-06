2197204d01c475a5ca641a78a99498390bb3d47c
Md Tonmoym <tonmoy52532@gmail.com>
2022-02-06 20:39:09 +0600

    Topbar

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ ob_end_flush();
   </div>
   <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="./"><?php echo $_SESSION['setting_name'] ?></a>
+      <a class="navbar-brand js-scroll-trigger" href="./">Doctor</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto my-2 my-lg-0">
